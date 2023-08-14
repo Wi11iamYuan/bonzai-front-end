cb9b88c8f23f903429bed0af1292bb45e39b784d
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2023-08-13 21:02:30 -0700

    home page in the works

--- front-end/src/components/TopNavbar.js
+++ front-end/src/components/TopNavbar.js
@@ -6,9 +6,9 @@ import Image from 'react-bootstrap/Image';
 
 export const TopNavbar = () => {
     return (
-    <Navbar collapseOnSelect expand="lg" className="bg-dark">
+    <Navbar collapseOnSelect expand="lg" fixed = "top" className="bg-dark" id="nav">
         <Image src="./logo_no_name.svg" id="navbar-icon"/>
-        <Navbar.Brand href="/home" className="text-light">Bonzai</Navbar.Brand>
+        <Navbar.Brand href="/home" className="text-light" id= 'brand'>Bonzai</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -18,10 +18,10 @@ export const TopNavbar = () => {
             <Nav.Link href="/info" className="text-light">Information</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/settings" id='navbar-settings' className="text-light">Settings</Nav.Link>
+            <Nav.Link href="/settings" id="navbar-settings" className="text-light">Settings</Nav.Link>
           </Nav>
         </Navbar.Collapse>
    
     </Navbar>
     );
-}
+} 
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,9 +1,41 @@
+import '../App.css';
+
 import { TopNavbar } from "../components/TopNavbar";
+import { Parallax } from 'react-parallax';
+
+import Image from 'react-bootstrap/Image';
+
+const PageOne = () =>{
+    return(
+        <div className="home-page-one" strength={800}>
+            <div id="page-one-info">
+                Hello, <br></br>
+                Welcome to Bonzai
+            </div>
+            <div>
+                <Image src = '../logo_no_name.svg' id="page-one-img"></Image>
+            </div>
+        </div>
+    );
+}
+
+const PageTwo = () =>{
+    return(
+        <div className="home-page-two" strength={800}>
+            <div id="page-two-info">
+                hey hey hey
+            </div>
+        </div>
+    );
+}
 
 export const HomePage = () => {
     return(
         <>
         <TopNavbar />
+        <PageOne />
+        <div className = "page-divider"></div>
+        <PageTwo />
         </>
     );
 }
